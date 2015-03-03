5c64cdddd07d07caa719423417987b586bd9112e
mpharrigan <harrigan@stanford.edu>
2015-03-02 21:11:33 -0800

    Authenticate

--- copy_to_corn.sh
+++ copy_to_corn.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Get the right python env
+source $HOME/.bashrc
+
 # change to directory of script
 cd "$(dirname "$0")"
 
+# Render html
 ./parse_log.py
+
+# Special authentication script:
+source authenticate.sh
+
+# Copy
 scp log.html log.content.html about.html corn:~/WWW/fah/ >> scp.log
